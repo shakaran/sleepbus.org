98a49bb0a0d84718917c7fc7d8369f627595005e
Leon <lionhive@gmail.com>
2017-06-27 10:52:01 +1200

    styling for volunteer thanks page

--- src/application/views/thanks/volunteer-signup-thanks.php
+++ src/application/views/thanks/volunteer-signup-thanks.php
@@ -1,11 +1,58 @@
-<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" style="margin-top:135px;">
   <div class="container">
     <div class="row">
-      <div class="funding">
-        <h1>You're Awesome!</h1>
-        <h2>Thank you for your support. We will be in touch shortly with more details.</h2>
-        <img src="thebus.jpg" />
-      </div>
-    </div>
+			<div class="col-md-2">
+				<img src="/images/simon_founder.png" style="height:343px;" />
+			</div>
+
+			<div class="col-md-8 text-center">
+        <h1 style="color:#2fc5f4;font-size:66px;font-weight:900;">YOU'RE AWESOME!</h1>
+
+        <p style="font-size:40px;">Thank you for your support.</p>
+
+				<p style="font-size:40px;">We will be in touch shortly with more details.</p>
+			</div>
+
+			<div class="col-md-2">
+			</div>
+		</div>
+
+    <div class="row">
+			<div class="col-md-12 text-center">
+				<p style="font-size:22px;"><b>Here are 6 ways you can get involved</b></p>
+			</div>
+		</div>
+
+    <div class="row">
+			<div class="col-md-2 ways-item">
+					<a href="/donate"><img src="images/Button_Donate.png" alt="img"></a>
+					<p>For $27.50 you can give a good night’s sleep.</p>
+			</div>
+
+			<div class="col-md-2 ways-item">
+					<a href="/pledge"><img src="images/Button_Pledge.png" alt="img"></a>
+					<p>Pledge your next birthday for safe sleeps.</p>
+			</div>
+
+			<div class="col-md-2 ways-item">
+					<a href="/fundraise"><img src="images/Button_Fundraise.png" alt="img"></a>
+					<p>Do something crazy or creative to raise money.</p>
+			</div>
+
+			<div class="col-md-2 ways-item">
+					<a href="/volunteer"><img src="images/Button_Volunteer.png" alt="img"></a>
+					<p>Jump onboard and lend a hand.</p>
+			</div>
+
+			<div class="col-md-2 ways-item">
+					<a href="/corporate-supporters"><img src="images/Button_Partnership.png" alt="img"></a>
+					<p>Together we can end people sleeping rough.</p>
+			</div>
+
+			<div class="col-md-2 ways-item">
+					<a href="/connect"><img src="images/Button_Connect.png" alt="img"></a>
+					<p>Introduce us to people, companies, councils & more.</p>
+			</div>
+		</div>
   </div>
 </div>
