8f31a38c40105fd7b4db34cf3688427489023238
Leon <lionhive@gmail.com>
2017-07-02 14:29:59 +1200

    styling volunteer thanks page
     - resized icons
     - adjusted styles
     - need better proportioned images

--- src/application/views/thanks/volunteer-signup-thanks.php
+++ src/application/views/thanks/volunteer-signup-thanks.php
@@ -2,7 +2,7 @@
   <div class="container">
     <div class="row">
 			<div class="col-md-2">
-				<img src="/images/simon_founder.png" style="height:343px;" />
+				<img src="/images/simon_thumbs_up.png" style="height:343px;" />
 			</div>
 
 			<div class="col-md-8 text-center">
@@ -19,14 +19,16 @@
 			</div>
 		</div>
 
-<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 donatehome6ways">
 	<div class="container">
-		<div class="row donateh2">
 			<h2>Here are 6 ways you can get involved</h2>
+	</div>
+</div>
+
 
 			<div class="col-lg-2 col-md-2 col-sm-2 col-xs-12 donatehome">
 				<div class="donatehomeimg">
-					<img alt="" src="/images/icon1.png" />
+					<img alt="" src="/images/icon_donate.png" />
 				</div>
 
 				<p>For $27.50 you can give a good night&rsquo;s sleep.</p>
@@ -35,7 +37,7 @@
 
 			<div class="col-lg-2 col-md-2 col-sm-2 col-xs-12 donatehome">
 				<div class="donatehomeimg">
-					<img alt="" src="/images/icon2.png" />
+					<img alt="" src="/images/icon_pledge.png" />
 				</div>
 
 				<p>Pledge your next Birthday for safe sleeps.</p>
@@ -44,7 +46,7 @@
 
 			<div class="col-lg-2 col-md-2 col-sm-2 col-xs-12 donatehome">
 				<div class="donatehomeimg">
-					<img alt="" src="/images/icon3.png" />
+					<img alt="" src="/images/icon_fundraise.png" />
 				</div>
 
 				<p>Do something crazy or creative to raise money.</p>
@@ -77,8 +79,6 @@
 				<p>Introduce us to people, companies, councils & more.</p>
 				<a class="btn btn-primary btn-connect" href="/connect">CONNECT</a>
 			</div>
-	</div>
-</div>
 
   </div>
 </div>
