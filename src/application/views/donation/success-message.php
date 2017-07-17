32379c222b0eb5e5384020ac618cc95c7b07bc83
Leon <lionhive@gmail.com>
2017-07-17 13:23:50 +0800

    show pledge advert on onetime thank you page

--- src/application/views/donation/success-message.php
+++ src/application/views/donation/success-message.php
@@ -2,9 +2,21 @@
   <div class="container">
     <div class="row">
 	<div class="funding">
-	<?php
-  	  echo $thanks['message'];
-    ?>
+
+<div class="col-lg-3 col-md-3 col-sm-3 col-xs-12" id="activecampaigns2">
+  <a href="https://www.sleepbus.org/pledge">
+    <img alt="" src="/images/pledge_advert.jpg" style="height:600px;" />
+  </a>
+</div>
+
+<div class="col-lg-8 col-md-8 col-sm-8 col-xs-12">
+<div class="fundingtoptext"><img alt="" src="/application/third_party/ckfinder/userfiles/images/Simon_Bitmoji_Thank_You_no_logo.jpeg" />
+<h2>Thank you so much for your donation and support. Once your funds have been allocated to a project&nbsp;we will let you know which one so you can follow its progress. Thanks again for your support&nbsp;:)</h2>
+</div>
+</div>
+
+
+
     </div>
        </div>
     </div>
