95acdf13a8e774bf45f79298dca3b0d89f76c323
Leon <lionhive@gmail.com>
2017-07-18 09:50:37 +0800

    additional email template revisions

--- src/application/views/email/password_reset.php
+++ src/application/views/email/password_reset.php
@@ -1,7 +1,3 @@
-<div style="border:1px #002d42 solid; width:680px;">
-  <div style="background:#002d42; padding:10px;"><img src="<?php echo base_url();?>images/logo.png" border="0"> </div>
-  <div style="padding:10px; background:#fff;">
-    <div style="color:#000; font-size:12px; font-family:Arial, Helvetica, sans-serif; line-height:18px; padding:0px">
 
 <p>Oops, forgot your password? Don&#39;t worry, it happens to us all. Let&#39;s get you a new one.</p>
 
@@ -14,7 +10,3 @@
 
 </p>
 
-</div>
-  </div>
-  <div style="padding:3px; background:#002d42;"></div>
-</div>
