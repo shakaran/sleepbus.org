96f98203f70d4bf6e90659cb0ef689ffbbeb073f
Leon <lionhive@gmail.com>
2017-07-20 10:41:46 +0800

    fix email layout for campaign update

--- src/application/views/email/campaign_update_to_donors.php
+++ src/application/views/email/campaign_update_to_donors.php
@@ -1,7 +1,3 @@
-<div style="border:1px #002d42 solid; width:680px;">
-  <div style="background:#002d42; padding:10px;"><img src="<?php echo base_url();?>images/logo.png" border="0"> </div>
-  <div style="padding:10px; background:#fff;">
-    <div style="color:#000; font-size:12px; font-family:Arial, Helvetica, sans-serif; line-height:18px; padding:0px">
 
 <p>Hey, new information is now available on the sleepbus campaign you&#39;re supporting. Login in now to see what&#39;s happening.</p>
 
@@ -23,7 +19,3 @@
       <div style='clear:both;'></div></div>
 
 
-</div>
-  </div>
-  <div style="padding:3px; background:#002d42;"></div>
-</div>
