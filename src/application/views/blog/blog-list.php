3bfb4ce48c1cbf8121dd85fd70e3986e77f5dbd3
Leon <lionhive@gmail.com>
2017-07-24 17:40:01 +0000

    link to july newsletter

--- src/application/views/blog/blog-list.php
+++ src/application/views/blog/blog-list.php
@@ -21,24 +21,44 @@
  <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 blogmainbox">
   <div class="row">
   
-	<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 blogbox">
-      <a href="<?php echo base_url().'blog/category/blog2';?>"> 
+    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 blogbox">
+        <a href="<?php echo base_url().'blog/july_2017_news';?>"> 
 
-        <div class="blogboxhover">
-        <p><span>an update</span></p>
+          <div class="blogboxhover">
+            <p><span>latest news</span></p>
 
-        <h2>Dear sleepbus family.</h2>
-        </div>
+            <h2>July 2017 Newsletter</h2>
+          </div>
 
-        <figure><img alt="" src="/images/img22.jpg" /></figure>
+          <figure><img alt="" src="/images/img22.jpg" /></figure>
 
-        <div class="redmore">
-        <p>&nbsp;</p>
-        </div>
+          <div class="redmore">
+            <p>&nbsp;</p>
+          </div>
 
 
-       </a>
+         </a>
     </div>
+
+    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 blogbox">
+        <a href="<?php echo base_url().'blog/category/blog2';?>"> 
+
+          <div class="blogboxhover">
+            <p><span>an update</span></p>
+
+            <h2>Dear sleepbus family.</h2>
+          </div>
+
+          <figure><img alt="" src="/images/img22.jpg" /></figure>
+
+          <div class="redmore">
+            <p>&nbsp;</p>
+          </div>
+
+
+         </a>
+    </div>
+
   </div>  
  </div>
 
