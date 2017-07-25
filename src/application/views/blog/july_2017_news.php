9ee088ed933fc21dc2e1a190dce236adb2184c99
Leon <lionhive@gmail.com>
2017-07-25 00:40:11 +0000

    title update on newsletter

--- src/application/views/blog/july_2017_news.php
+++ src/application/views/blog/july_2017_news.php
@@ -14,7 +14,7 @@
 <?php endif; ?>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Sleepbus.org</title>
+	<title>July 2017 Newsletter | sleepbus</title>
 	<link media="all" rel="stylesheet" href="/style/main.css">
 </head>
 <body>
