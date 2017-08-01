e9d9debff0a60f3131cb8ac03a5f09f72df5ce66
Ángel Guzmán Maeso <shakaran@gmail.com>
2017-08-01 15:50:23 +0200

    Apply the same sugar syntax to duplicated code

--- kill_containers.sh
+++ kill_containers.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
 # convenience script to destroy any running containers
-sudo docker rm -f sleepbussql
-sudo docker rm -f sleepbusweb
-sudo docker rm -f mailcatcher
-sudo docker rm -f phpmyadmin
+sudo docker rm -f {sleepbussql,sleepbusweb,mailcatcher,phpmyadmin}
